4302dadbba3e8f3f059bbc9e66467eb029a6d371
caiolarroza <caiolarroza@hotmail.com>
2019-01-22 01:40:19 +0000

    Finalizando aula [023]

---
+++ src/components/memory-card/index.js
@@ -0,0 +1,30 @@
+function createMemoryCard() {
+    
+    const $memoryCard = document.createElement("article");
+
+    const $iconCollab = `<img 
+    src='img/icon-collabcode.png' 
+    alt='Gueio mascote da CollabCode' 
+    class='icon'>`;
+    
+    $memoryCard.classList.add("memory-card");
+    $wrapCards.insertBefore($memoryCard, null);
+    
+    $memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
+}
+
+function createMemoryCardFront() {
+
+    const $memoryCardFront = document.createElement("article");
+
+    const $iconJs = `<img 
+    src='img/js.png' 
+    alt='Gueio mascote da CollabCode' 
+    class='icon'>`;
+
+    $memoryCardFront.classList.add("memory-card");
+    $memoryCardFront.classList.add("-front");
+    $wrapCards.insertBefore($memoryCardFront, null);
+
+    $memoryCardFront.insertAdjacentHTML("afterbegin", $iconJs);
+}
