a2c5108d71ab01016458f06100f2426fac343fd1
caiolarroza <caiolarroza@hotmail.com>
2019-01-25 14:39:54 +0000

    Finalizando aula [026]

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,11 +1,14 @@
-const createMemoryCard = (cardObj) => {
-    console.log(cardObj.alt);
+//object destructuring já recebendo como parãmentro as variáveis separadas
+const createMemoryCard = ({src, alt, nameClass}) => {
+    
+    //object destructuring
+    // const {src, alt, nameClass} = cardObj;
     
     return `
-        <article class="memory-card ${cardObj.nameClass}">
+        <article class="memory-card ${nameClass}">
             <img 
-                src="${cardObj.src}" 
-                alt="${cardObj.alt}" 
+                src="${src}" 
+                alt="${alt}" 
                 class="icon"
                 onClick="handleClick()"
             />
