3322d78a6c701b019b34e9e10d0c990dbf7f7d26
caiolarroza <caiolarroza@hotmail.com>
2019-01-25 14:47:53 +0000

    [026] - Desafio Único

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,11 +1,14 @@
-const createMemoryCard = (cardObj) => {
-    console.log(cardObj.alt);
+//object destructuring já recebendo como parãmentro as variáveis separadas
+const createMemoryCard = ({src, alt, nameClass}) => {
+    
+    //object destructuring
+    // const {src, alt, nameClass} = cardObj;
     
     return `
-        <article class="memory-card ${cardObj.nameClass}">
+        <article class="memory-card ${nameClass}">
             <img 
-                src="${cardObj.src}" 
-                alt="${cardObj.alt}" 
+                src="${src}" 
+                alt="${alt}" 
                 class="icon"
                 onClick="handleClick()"
             />
@@ -15,3 +18,67 @@ const createMemoryCard = (cardObj) => {
 function handleClick() {
     console.log("click");
 }
+
+const $head = document.querySelector("head");
+const $styleCard = document.createElement("style");
+
+$styleCard.textContent = `
+    .memory-card {
+        width: 155px;
+        height: 155px;
+        background-color: #f25a70;
+        border-radius: 30px;
+        display: flex;
+        /* display: inline-flex; minha forma de fazer */
+        justify-content: center;
+        align-items: center;
+        box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16);
+        position: relative;
+        cursor: pointer;
+        /* margin: 16px;  minha forma de fazer*/ 
+    }
+
+    .memory-card.-front {
+        background-color: #fff;
+    }
+
+    .memory-card.-front::before {
+        content: "";
+        width: 95px;
+        height: 95px;
+        background-color: #d4d4d4;
+        border-radius: 50%;
+        position: absolute;
+    }
+
+    .memory-card > .icon {
+        width: 100px;
+        height: 100px;
+    }
+
+    .memory-card.-front > .icon {
+        /* position: absolute; minha forma de fazer*/
+        transform: translateY(-12px);
+    }
+
+    /* forma que fiz */
+
+    /* @media (width: 375px) {
+        .memory-card {
+            width: 95px;
+            height: 95px;
+            margin: 16px 46px;
+        }
+
+        .memory-card.-front::before {
+            width: 50px;
+            height: 50px;
+        }
+        
+        .memory-card > .icon {
+            width: 50px;
+            height: 50px;
+        }
+    } */`;
+
+    $head.insertBefore($styleCard, null)
--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -2,27 +2,21 @@ const $root = document.querySelector("#root");
 
 const $cardsWrapper = createCardsWrapper();
 
-let bugObj = {src: "img/bug.png", alt: "Ícone de um bug", nameClass: "-front"};
-let respObj = {src: "img/responsivo.png", alt: "Ícone de responsividade", nameClass: "-front"};
-let javaObj = {src: "img/java.png", alt: "Ícone de um livro de Java", nameClass: "-front"};
-let jsObj = {src: "img/js.png", alt: "Ícone de um livro de JS", nameClass: "-front"};
-let gueioObj = {src: "img/icon-collabcode.png", alt: "Gueio mascote da CollabCode"};
-
-const $memoryCardFrontBug = createMemoryCard(bugObj);
-const $memoryCardFrontResp = createMemoryCard(respObj);
-const $memoryCardFrontJava = createMemoryCard(javaObj);
-const $memoryCardFrontJs = createMemoryCard(jsObj);
-const $memoryCardGueio = createMemoryCard(gueioObj);
+const $memoryCardBug = createMemoryCard({src: "img/bug.png", alt: "Ícone de um bug", nameClass: "-front"});
+const $memoryCardResp = createMemoryCard({src: "img/responsivo.png", alt: "Ícone de responsividade", nameClass: "-front"});
+const $memoryCardJava = createMemoryCard({src: "img/java.png", alt: "Ícone de um livro de Java", nameClass: "-front"});
+const $memoryCardJS = createMemoryCard({src: "img/js.png", alt: "Ícone de um livro de JS", nameClass: "-front"});
+const $memoryCardGueio = createMemoryCard({src: "img/icon-collabcode.png", alt: "Gueio mascote da CollabCode"});
 
 
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFrontJs);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFrontJs);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFrontBug);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFrontBug);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFrontResp);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFrontResp);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFrontJava);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFrontJava);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJS);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJS);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardBug);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardBug);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardResp);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardResp);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJava);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJava);
 $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardGueio);
 $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardGueio);
 
