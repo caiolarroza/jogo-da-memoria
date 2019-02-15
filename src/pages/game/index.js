c593f739b2e900bf5119095be556d6c0af502ef2
caiolarroza <caiolarroza@hotmail.com>
2019-02-15 22:05:30 +0000

    Finalizando aula [033]

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -6,14 +6,13 @@
     
     const $cardsWrapper = createCardsWrapper();
     const createMemoryCard = memoryCard.create();
-
     
     const $memoryCardBug = createMemoryCard({src: "img/bug.png", alt: "Ícone de um bug"});
     const $memoryCardResp = createMemoryCard({src: "img/responsivo.png", alt: "Ícone de responsividade"});
     const $memoryCardJava = createMemoryCard({src: "img/java.png", alt: "Ícone de um livro de Java"});
     const $memoryCardJS = createMemoryCard({src: "img/js.png", alt: "Ícone de um livro de JS"});
     const $memoryCardGueio = createMemoryCard({src: "img/icon-collabcode.png", alt: "Gueio mascote da CollabCode"});
-    
+    const $pointBar = pointBar.create();
     
     $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJS);
     $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJS);
@@ -25,4 +24,5 @@
     $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJava);
     
     $root.insertAdjacentElement("beforeend", $cardsWrapper);
+    $root.insertAdjacentHTML("afterbegin", $pointBar);
 })();
